de3f00c9c0307480fbfb32e2d42660d0d86d858f
Vasily Himchenko <White_Scorpion@inbox.lv>
2023-01-24 11:38:13 +0200

    Add project files.

---
+++ Angular2021CourseAPI/Controllers/DataApiController.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Angular2021CourseAPI.Controllers
+{
+    /// <summary>
+    /// The data api controller.
+    /// </summary>
+    [ApiController]
+    [Route("[controller]")]
+    public class DataApiController : Controller
+    {
+        private readonly ILogger<DataApiController> _logger;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DataApiController"/> class.
+        /// </summary>
+        /// <param name="logger">The logger.</param>
+        public DataApiController(ILogger<DataApiController> logger)
+        {
+            _logger = logger;
+        }
+
+    }
+}
